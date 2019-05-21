409cabebe7ef27519b87beb9d67b3301479a6896
ハヤ斗スギタ <0418ya@gmail.com>
2019-05-21 19:26:44 +0900

    recollect 必須 for comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show] do
     get 'confirm', on: :collection, to: 'users#confirm'
