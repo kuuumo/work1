a4076a2a613016d736e5734795a4246c56add046
ハヤ斗スギタ <0418ya@gmail.com>
2019-06-08 07:00:03 +0900

    branch切り替え

---
+++ testscript.sh
@@ -0,0 +1,6 @@
+#!/ba#!/bin/bash
+GITHUB_TOKEN=1a015348001658fb406168f0cd06f826ce2e831f
+GITHUB_BASEURL=https://api.github.com
+GITHUB_API=/repos/YourOrganization/YourProject/pulls?state=open
+
+curl -k -s -u :$GITHUB_TOKEN $GITHUB_BASEURL$GITHUB_AP
